9fd0c87273825ffef154dced2d03c1d088e8376a
nik0zer <s21b1.nikozer@gmail.com>
2024-02-06 19:52:10 +0300

    #

---
+++ 1/build/CMakeFiles/parproga_1.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nikozer/univercity_tasks/parproga/1/code/src/main.cpp" "CMakeFiles/parproga_1.dir/code/src/main.cpp.o" "gcc" "CMakeFiles/parproga_1.dir/code/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 1/build/CMakeFiles/parproga_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parproga_1.dir/code/src/main.cpp.o"
+  "CMakeFiles/parproga_1.dir/code/src/main.cpp.o.d"
+  "parproga_1"
+  "parproga_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parproga_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
